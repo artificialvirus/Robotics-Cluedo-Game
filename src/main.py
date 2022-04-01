e11c175584ce43638f99495d2a58d64b6d8ae9ed
Alper Onder <sc19ao@leeds.ac.uk>
2022-04-01 13:34:31 +0100

    commit

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 import rospy
+import numpy as np
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import actionlib
+from actionlib_msgs.msg import *
+from geometry_msgs.msg import Pose, Point, Quaternion
 
 if __name__ == '__main__':
     # Your code should go here. You can break your code into several files and
-    # include them in this file. You just need to make sure that your solution 
+    # include them in this file. You just need to make sure that your solution
     # can run by just running rosrun group_project main.py
     pass
