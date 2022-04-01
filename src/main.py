a94a2bd411a7c6a4a7ba51f9bd683444f03388b9
Alexander Pitsin <sc19akp@leeds.ac.uk>
2022-04-01 14:25:31 +0000

    Launch dir and movement

--- src/main.py
+++ src/main.py
@@ -8,6 +8,61 @@ import sys
 from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+
+
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import actionlib
+from actionlib_msgs.msg import *
+from geometry_msgs.msg import Pose, Point, Quaternion
+
+class GoToPose():
+    def __init__(self):
+
+        self.goal_sent = False
+
+	# What to do if shut down (e.g. Ctrl-C or failure)
+	rospy.on_shutdown(self.shutdown)
+	
+	# Tell the action client that we want to spin a thread by default
+	self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+	rospy.loginfo("Wait for the action server to come up")
+
+	self.move_base.wait_for_server()
+
+    def goto(self, pos, quat):
+
+        # Send a goal
+        self.goal_sent = True
+	goal = MoveBaseGoal()
+	goal.target_pose.header.frame_id = 'map'
+	goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose = Pose(Point(pos['x'], pos['y'], 0.000),
+                                     Quaternion(quat['r1'], quat['r2'], quat['r3'], quat['r4']))
+
+	# Start moving
+        self.move_base.send_goal(goal)
+
+	# Allow TurtleBot up to 60 seconds to complete task
+	success = self.move_base.wait_for_result(rospy.Duration(60)) 
+
+        state = self.move_base.get_state()
+        result = False
+
+        if success and state == GoalStatus.SUCCEEDED:
+            # We made it!
+            result = True
+        else:
+            self.move_base.cancel_goal()
+
+        self.goal_sent = False
+        return result
+
+    def shutdown(self):
+        if self.goal_sent:
+            self.move_base.cancel_goal()
+        rospy.loginfo("Stop")
+        rospy.sleep(1)
+
 class colourIdentifier():
 
     def __init__(self):
@@ -65,18 +120,38 @@ class colourIdentifier():
 def main(args):
     # Instantiate your class
     # And rospy.init the entire node
-    cI = colourIdentifier()
+    # cI = colourIdentifier()
     # Ensure that the node continues running with rospy.spin()
     # You may need to wrap rospy.spin() in an exception handler in case of KeyboardInterrupts
-    rospy.init_node('colourIdentifier', anonymous=True)
+    # rospy.init_node('colourIdentifier', anonymous=True)
+    #-1.43, 1.15
     try:
-        rospy.spin()
+        rospy.init_node('nav_test', anonymous=True)
+        navigator = GoToPose()
+
+        # Customize the following values so they are appropriate for your location
+        #-2.3, 5.63
+        x = -2.3 #-5.0 # SPECIFY X COORDINATE HERE
+        y = 5.63 #-0.2 # SPECIFY Y COORDINATE HERE
+        theta = 0 # 30# SPECIFY THETA (ROTATION) HERE
+        position = {'x': x, 'y' : y}
+        quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : np.sin(theta/2.0), 'r4' : np.cos(theta/2.0)}
+
+        rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
+        success = navigator.goto(position, quaternion)
+
+        if success:
+            rospy.loginfo("Hooray, reached the desired pose")
+        else:
+            rospy.loginfo("The base failed to reach the desired pose")
+        # rospy.spin()
     except KeyboardInterrupt:
         print("Shutting down")
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main(sys.argv)
+
 
     # Your code should go here. You can break your code into several files and
     # include them in this file. You just need to make sure that your solution
